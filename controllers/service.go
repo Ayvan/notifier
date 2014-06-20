7c7179a0caf486fa4e172d9e4bb6a4363219700a
Ayvan <ajvan.ivan@gmail.com>
2014-06-20 18:27:20 +0400

    @fix for

--- controllers/service.go
+++ controllers/service.go
@@ -29,39 +29,43 @@ func (this *ServiceController) DbCleaner(noticeCleanChan chan models.Notice) {
 }
 
 func (this *ServiceController) NoticeWorker(noticeChan chan models.Notice, messageChan chan models.Message) {
-	<- noticeChan
-	fmt.Println("Notice worker ok!")
-	message := models.Message{1}
-	messageChan <- message
-	/**
+	for {
+		<-noticeChan
+		fmt.Println("Notice worker ok!")
+		message := models.Message{1}
+		messageChan <- message
+		/**
 	обрабатывает полученный notice
 	запрашивает у канала список пользователей
 	определяет список пользователей, кому его отправить
 	отправляет в MessageWorker
 	 */
-
+	}
 }
 
 func (this *ServiceController) MessageWorker(messageChan chan models.Message, channelMessageChan chan models.ChannelMessage) {
-	<- messageChan
-	fmt.Println("Message worker ok!")
-	channelMessage := models.ChannelMessage{1}
-	channelMessageChan <- channelMessage
-	/**
+	for {<-messageChan
+		fmt.Println("Message worker ok!")
+		channelMessage := models.ChannelMessage{1}
+		channelMessageChan <- channelMessage
+		/**
 	получает пользователя (ID) кому отправить
 	запрашивает у UserModel список каналов
 	отправляет в ChannelDispatcher сообщение и номер канала
 	 */
+	}
 }
 
 func (this *ServiceController) ChannelDispatcher(channelMessageChan chan models.ChannelMessage) {
-	<- channelMessageChan
-	fmt.Println("Channel dispatcher ok!")
-	/**
+	for {
+		<-channelMessageChan
+		fmt.Println("Channel dispatcher ok!")
+		/**
 	создает chan для всех каналов (по 1 на канал)
 	запускает ChannelMessageWorker'ы (от 1 до многих на каждый chan)
 	роутит сообщения в каналы, прослушиваемые несколькими (от 1 на канал) ChannelMessageWorker
 	 */
+	}
 }
 
 func (this *ServiceController) ChannelMessageWorker() {
