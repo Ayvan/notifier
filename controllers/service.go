d19615c4ecb38d27925d949c365bd6412be0af03
Ayvan <ajvan.ivan@gmail.com>
2014-06-20 19:41:58 +0400

    @comments описание процесса в комментариях

--- controllers/service.go
+++ controllers/service.go
@@ -11,6 +11,10 @@ type ServiceController struct {
 	beego.Controller
 }
 
+/**
+	Читатель БД, он запрашивает в БД уведомления, которые надо отправить в ближайшее время,
+	отправляет их дальше, а также решает, удаляем ли это сообщение из БД или нет, удаляемые отправляет в noticeCleanChan
+ */
 func (this *ServiceController) DbReader(noticeChan chan models.Notice, noticeCleanChan chan models.Notice) {
 	ch := time.Tick(2 * time.Second)
 	notice := models.Notice{1}
@@ -24,6 +28,9 @@ func (this *ServiceController) DbReader(noticeChan chan models.Notice, noticeCle
 	}
 }
 
+/**
+	"Чистильщик" БД, получает из chan уведомления и удаляет их
+ */
 func (this *ServiceController) DbCleaner(noticeCleanChan chan models.Notice) {
 	for {
 		<-noticeCleanChan
@@ -31,6 +38,9 @@ func (this *ServiceController) DbCleaner(noticeCleanChan chan models.Notice) {
 	}
 }
 
+/**
+	Обработчик уведомлений: получает уведомление, из Group получает список пользователей и отправляет им сообщения
+ */
 func (this *ServiceController) NoticeWorker(noticeChan chan models.Notice, messageChan chan models.Message) {
 	for {
 		<-noticeChan
@@ -38,39 +48,51 @@ func (this *ServiceController) NoticeWorker(noticeChan chan models.Notice, messa
 		message := models.Message{1}
 		messageChan <- message
 		/**
-	обрабатывает полученный notice
-	запрашивает у канала список пользователей
-	определяет список пользователей, кому его отправить
-	отправляет в MessageWorker
-	 */
+			обрабатывает полученный notice
+			запрашивает у группы список пользователей
+			определяет список пользователей, кому его отправить
+			отправляет в MessageWorker
+	 	*/
 	}
 }
 
+/**
+	Обработчик сообщений: получает сообщения и ID пользователей, из User получает список каналов и их параметры
+	 и отправляет сообщения	в соответствующие каналы, передавая адрес получателя (телефон, email и т.д.)
+ */
 func (this *ServiceController) MessageWorker(messageChan chan models.Message, channelMessageChan chan models.ChannelMessage) {
 	for {<-messageChan
 		fmt.Println("Message worker ok!")
 		channelMessage := models.ChannelMessage{1}
 		channelMessageChan <- channelMessage
 		/**
-	получает пользователя (ID) кому отправить
-	запрашивает у UserModel список каналов
-	отправляет в ChannelDispatcher сообщение и номер канала
-	 */
+			получает пользователя (ID) кому отправить
+			запрашивает у UserModel список каналов
+			отправляет в ChannelDispatcher сообщение и номер канала
+	 	*/
 	}
 }
 
+/**
+	Диспетчер каналов: он знает обо всех каналах, создает для них набор chan (по 1 на канал) и запускает воркеры,
+	которые будут обрабатывать сообщения, адресованные их каналам
+ */
 func (this *ServiceController) ChannelDispatcher(channelMessageChan chan models.ChannelMessage) {
 	for {
 		<-channelMessageChan
 		fmt.Println("Channel dispatcher ok!")
 		/**
-	создает chan для всех каналов (по 1 на канал)
-	запускает ChannelMessageWorker'ы (от 1 до многих на каждый chan)
-	роутит сообщения в каналы, прослушиваемые несколькими (от 1 на канал) ChannelMessageWorker
-	 */
+			создает chan для всех каналов (по 1 на канал)
+			запускает ChannelMessageWorker'ы (от 1 до многих на каждый chan)
+			роутит сообщения в каналы, прослушиваемые несколькими (от 1 на канал) ChannelMessageWorker
+	 	*/
 	}
 }
 
+/**
+	Обработчик сообщений, отправленных в канал: получает адрес и сообщение, запускает метод Channel.Send()
+	 Метод Channel.Send() должен отформатировать сообщение согласно правилам канала и вызывать соответствующий сервис-провайдер
+ */
 func (this *ServiceController) ChannelMessageWorker() {
 
 }
--- main.go
+++ main.go
@@ -15,10 +15,17 @@ func startService() {
 	messageChan := make(chan models.Message, 100)
 	channelMessageChan := make(chan models.ChannelMessage, 100)
 
+	//запускаем процесс, читающий БД
 	go c.DbReader(noticeChan,noticeCleanChan)
+	//запускаем процесс, удаляющий из БД обработанные записи
 	go c.DbCleaner(noticeCleanChan)
+	//запускаем воркер уведомлений: он обрабатывает уведомление и решает кому его отправить
 	go c.NoticeWorker(noticeChan,messageChan)
+	//запусукаем воркер сообщений: он получает сообщение и ID получателя (юзера)
+	//запрашивает у User список каналов и отправляет диспетчеру каналов сообщение и идентификатор канала
 	go c.MessageWorker(messageChan, channelMessageChan)
+	//запусаем диспетчер каналов
+	//создает chan для каждого канала и воркеры для обработки этих chan
 	go c.ChannelDispatcher(channelMessageChan)
 }
 
